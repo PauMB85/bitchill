f95f49f62e2855f6b9b1162b690a203594c41557
Pau MB <Pau MB>
2024-01-28 13:54:10 +0100

    fix title

--- front/src/components/title/TitleLanding.jsx
+++ front/src/components/title/TitleLanding.jsx
@@ -3,7 +3,9 @@ import { Typography } from '@mui/material';
 function TitleLanding() {
 	return (
 		<>
-			<Typography variant='h2'>Ahorra a tu ritmo con Bitcoin.</Typography>
+			<Typography variant='h2'>
+				Ahorra a tu ritmo <br /> con Bitcoin.
+			</Typography>
 			<Typography variant='h5'>
 				El mejor Dollar Cost Average con rendimientos que te abre una linea de
 				crédito inmediata.
