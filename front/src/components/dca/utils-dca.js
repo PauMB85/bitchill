fc07579d6c72478667b5b09a0522b92c516d4ce8
Pau MB <Pau MB>
2024-01-28 09:54:08 +0100

    approve

---
+++ front/src/components/dca/utils-dca.js
@@ -0,0 +1,44 @@
+export const listaCantidad = [
+	{
+		value: 50,
+		label: '50 USD',
+	},
+	{
+		value: 100,
+		label: '100 USD',
+	},
+	{
+		value: 200,
+		label: '200 USD',
+	},
+];
+
+export const listaFrequencia = [
+	{
+		value: 4,
+		label: 'Semanal',
+	},
+	{
+		value: 2,
+		label: 'Quincenal',
+	},
+	{
+		value: 1,
+		label: 'Mensual',
+	},
+];
+
+export const listaDuracion = [
+	{
+		value: 12,
+		label: '1 Año',
+	},
+	{
+		value: 24,
+		label: '2 Años',
+	},
+	{
+		value: 60,
+		label: '5 Años',
+	},
+];
