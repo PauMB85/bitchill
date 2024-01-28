c3d10f4e5fc3c85fcbf064ccd77984a69a4cf0f9
Pau MB <Pau MB>
2024-01-28 10:57:33 +0100

    redirect logout

--- front/src/components/logout/Logout.jsx
+++ front/src/components/logout/Logout.jsx
@@ -1,16 +1,23 @@
 import { Button } from '@mui/material';
 import { useContext } from 'react';
 import { Web3Context } from '../../context/Web3Context';
+import { useNavigate } from 'react-router-dom';
 
 export default function Logout() {
 	const { setProvider, web3auth, setWeb3auth } = useContext(Web3Context);
+	const navigateTo = useNavigate();
 	const logout = async () => {
 		if (!web3auth) {
 			console.error('Error al desconectar');
 		}
-		await web3auth.logout();
+		try {
+			await web3auth.logout();
+		} catch (err) {
+			console.error(err);
+		}
 		setProvider(null);
 		setWeb3auth(null);
+		navigateTo('/landing');
 	};
 	return <Button onClick={logout}>Logout</Button>;
 }
