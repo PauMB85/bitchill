51b72076b842339b29eb01699493bfd4e3788a7a
Pau MB <Pau MB>
2024-02-02 21:57:50 +0100

    landing responsive

--- front/src/pages/Landing.jsx
+++ front/src/pages/Landing.jsx
@@ -4,16 +4,19 @@ import LoginWithoutModal from '../components/login/LoginWithoutModal';
 import TitleLanding from '../components/title/TitleLanding';
 import Logo from '../components/logo/Logo';
 
+import useMediaQuery from '@mui/material/useMediaQuery';
+
 export default function Landing() {
+	const isMobile = useMediaQuery('(max-width:600px)');
 	return (
 		<Stack direction={'column'}>
 			<Logo />
 			<Stack
-				direction='row'
+				direction={isMobile ? 'column' : 'row'}
 				justifyContent='center'
 				alignItems='flex-start'
 				spacing={2}
-				sx={{ minHeight: '100vh', marginTop: '25px' }}
+				sx={{ minHeight: '100vh', marginTop: isMobile ? '5px' : '25px' }}
 			>
 				<Stack
 					direction='column'
